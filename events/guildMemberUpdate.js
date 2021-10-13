9438bb026dc3462f5a83aba121a78c23534fc700
drethenrain <drethenrain@hotmail.com>
2021-10-13 10:08:19 -0300

    remaking

--- events/guildMemberUpdate.js
+++
@@ -1,6 +0,0 @@
-const guildDB = require('../models/guild');
-
-exports.name = 'guildMemberUpdate';
-exports.run = async (client, member) => {
- // TO DO
-};
--- events/guildUpdate.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-const config = require("../config.json")
-
-exports.name = "guildUpdate";
-exports.run = (client, oldGuild, newGuild) => {
-  if (oldGuild.name !== newGuild.name) {
-    client.updateGuild(newGuild, { guildName: newGuild.name })
-  }
-};
