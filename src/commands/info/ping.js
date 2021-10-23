3410d2b41d2d037bb8e98bf4497e0eb7d64abe85
drethenrain <drethenrain@hotmail.com>
2021-10-22 23:12:23 -0300

    🔍 info category

--- src/commands/info/ping.js
+++ src/commands/info/ping.js
@@ -4,8 +4,6 @@ module.exports = {
     aliases: []
   },
   run: async (client, message, args) => {
-    message.reply({
-      content: `:ping_pong:  ${Math.round(client.ws.ping)}ms`
-    });
+    message.reply(`:ping_pong:  ${Math.round(client.ws.ping)}ms`);
   }
 };
